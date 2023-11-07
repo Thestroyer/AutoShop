de270f03bb0030f4cc46dc206f4759343ab5bb41
kuba <kubafig04@gmail.com>
2023-11-07 14:04:55 +0100

    nowe filesy: index.html, main.css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header><h1>AutoShop<h1></header>
+    <main>
+        <section class="wyroznione">
+            <h3>Wyróżnione ogłoszenia</h3>
+        </section>
+        <section class="ogloszenia">
+            
+        </section>
+    </main>
+</body>
+</html>
