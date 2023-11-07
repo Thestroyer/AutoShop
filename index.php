5094d04a58e228b2ddd5a611e3bf38bef2138c60
kuba <kubafig04@gmail.com>
2023-11-07 15:22:34 +0100

    zmiana styli

--- index.php
+++ index.php
@@ -19,12 +19,13 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body>
+    <h1>AutoShop</h1>
     <header>
-        <h1>AutoShop<h1>
         <div class="wyszukiwarka">
             <input class="wyszukiwarka-pole">
             <button class="wyszukiwarka-button material-symbols-outlined">search</button>
         </div>
+        <div class="koszyk material-symbols-outlined">shopping_cart</div>
     </header>
     <main>
         <section class="promocje"></section>
